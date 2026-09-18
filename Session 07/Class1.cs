ffdb7104ea6733b261d5b36efb58e5d30a774908
tarik31osama-afk <tarik31osama@gmail.com>
2026-09-18 17:49:30 +0300

    Add project files.

---
+++ Session 07/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Session_07
+{
+    internal class Class1
+    {
+    }
+}
---
+++ Session 07/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
